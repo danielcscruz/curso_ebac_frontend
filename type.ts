21c6f0fa7f1441cb95aca9dc108b03a9717f7ca9
Dan Cruz <danielcscruz@gmail.com>
2025-02-01 16:28:36 -0300

    first commit

---
+++ type.ts
@@ -0,0 +1,10 @@
+function multiplicar(a: number, b: number): number {
+    return a * b;
+}
+
+function ola(nome: string): string {
+    return "Olá " + nome;
+}
+
+console.log(multiplicar(15, 9)); 
+console.log(ola("Carlos")); 
